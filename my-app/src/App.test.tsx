2d320841401010334a50d16059b596213b5370bc
tolanchik1987 <tolanchik1987@mail.ru>
2022-10-31 12:07:39 +0400

    add test in App

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-describe('App component', () => {
-  	test('renders learn react link', () => {
+describe('App component', () => {              // Группа тестов
+  	test('renders learn react link', () => {         
     	render(<App />);
-		expect(screen.getByText(/description 2/i)).toBeInTheDocument();
-   		expect(screen.getByText(/Тестовое задание/i)).toBeInTheDocument();
+		expect(screen.getByText(/description 2/i)).toBeInTheDocument();    // Текст присутствует на странице
+   	expect(screen.getByText(/Тестовое задание/i)).toBeInTheDocument();   // Текст присутствует на странице 
   	});
 	test('renders and not checkbox', () => {
 		render(<App />);
-	  	expect(screen.queryByRole('checkbox'))
+	  	expect(screen.queryByRole('checkbox')).toBeNull()  //Элемента нет на странице
 	 });
+	 test('App snapshot', () => {
+		const app = render(<App />)
+		expect(app).toMatchSnapshot()          // Создает папку _snapshots_ и записывает в файл данные snapshot (проверяет что файл не изменился)
+	 });
+	 test('App input placeholder text', () => {
+		render(<App />);
+		expect(screen.getByPlaceholderText(/Whats need to be done/i)).toBeInTheDocument() // Проверяет текст по умолчанию в инпуте (placeholder)
+	 })
 });
